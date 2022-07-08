e4641f7d83a2444c3b48ac286cfd9cab2c3e4477
Maicon Romano <shardersfx@gmail.com>
2022-07-08 09:35:06 -0300

    Perguntas Frequentes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar Itens do Orçamento
-
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -23,3 +22,22 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
